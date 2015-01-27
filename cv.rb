4bed95dffd02e888296ad4263f94bc2b5abddfd0
Marin le Maignan <marin@seriousfox.co.uk>
2015-01-27 12:44:27 +0000

    made the mock up executable and readable in console

--- cv.rb
+++ cv.rb
@@ -1,32 +1,64 @@
-class MarinleMaignan < SuperGreat::BackEndDeveloper::ForSale
-  include "rails_best_practices"
+class MarinleMaignan
 
   attr_accessor :first_name, :last_name, :age, :email, :phone, :experience,
                 :diplomas, :skills, :future, :super_developer, :to_do_next
 
   def initialize(super_developer)
+    puts "HELLO WORLD !"
+    puts "\n"
+
     @first_name       = "Marin"
     @last_name        = "le Maignan"
-    @age              =  25
+    @age              =  "05-12-1989 (25)"
     @email            = "marin.lemaignan@gmail.com"
     @phone            = "07593085329"
 
-    @experience       = describe_work_experience
-    @diplomas         = describe_diplomas
-    @skills           = describe_skills
+    self.instance_variables.each do |var|
+      var = var.to_s
+      var.slice!(0)
+      puts send(var)
+    end
 
-    @to_do_next       = buy_the_guy!(super_developer)
+    ["work_experience", "diplomas", "skills"].each do |object|
+      puts "\n"
+      puts "\n"
+      puts "#{object.upcase}:"
+      describe self.send(object), object
+    end
   end
 
-  def describe_skills
-    [ :ruby, :ruby_on_rails, :javascript, :jquery, :html5, :css3,
-      :ui_design, :ux_design, :back_end, :front_end, :emberjs,
-      :git, :oop, :agile, :rspec, :bdd, :tdd, :deployment, :sql,
-      :cloud_technologies, :rvm, :heroku, :aws, :stripejs,
-      :data_manipulation, :pair_programming ]
+  def skills
+    [ [:ruby,                 "Very Good"],
+      [:ruby_on_rails,        "Very Good"],
+      [:javascript,           "Very Good"],
+      [:jquery,               "Very Good"],
+      [:html5,                "Very Good"],
+      [:css3,                 "Very Good"],
+      [:ui_design,            "Very Good"],
+      [:ux_design,            "Very Good"],
+      [:back_end,             "Very Good"],
+      [:front_end,            "Very Good"],
+      [:emberjs,              "Very Good"],
+      [:git,                  "Very Good"],
+      [:oop,                  "Very Good"],
+      [:agile,                "Very Good"],
+      [:rspec,                "Very Good"],
+      [:bdd,                  "Very Good"],
+      [:tdd,                  "Very Good"],
+      [:deployment,           "Very Good"],
+      [:sql,                  "Very Good"],
+      [:cloud_technologies,   "Very Good"],
+      [:rvm,                  "Very Good"],
+      [:heroku,               "Very Good"],
+      [:aws,                  "Very Good"],
+      [:stripejs,             "Very Good"],
+      [:data_manipulation,    "Very Good"],
+      [:pair_programming,     "Very Good"]
+    ]
+
   end
 
-  def describe_work_experience
+  def work_experience
     [{from:             2013,
       to:               2015,
       company:          "Serious Fox",
@@ -56,7 +88,7 @@ class MarinleMaignan < SuperGreat::BackEndDeveloper::ForSale
       city:             "Paris" }]
   end
 
-  def describe_diplomas
+  def diplomas
     [{from:             2011,
       to:               2013,
       title_awarded:    "Graphic Design Ba(Hons) 2/1",
@@ -70,6 +102,23 @@ class MarinleMaignan < SuperGreat::BackEndDeveloper::ForSale
       city:             "Paris" }]
   end
 
+  def describe(object, klass)
+    object.each_with_index do |exp, i|
+      i = i+1
+      if klass == "skills"
+        puts "- #{exp[0].capitalize} : #{exp[1].capitalize}"
+
+      else
+        puts "------------------------##{i}------------------------"
+        exp.each do |key, value|
+          puts "- #{key.capitalize} : #{value}"
+        end
+
+      end
+      puts "\n"
+    end
+  end
+
   def buy_the_guy!(super_developer)
     you = Employer.new()
 
@@ -81,3 +130,10 @@ class MarinleMaignan < SuperGreat::BackEndDeveloper::ForSale
     end
   end
 end
+
+
+
+a = MarinleMaignan.new("a")
+
+
+puts "Thank You for Reading!"
